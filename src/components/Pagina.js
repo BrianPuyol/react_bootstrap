929e0e5a260e68935be3b7f3648955713856b061
BrianPuyol <brianpuyol1997@gmail.com>
2024-07-23 14:52:52 -0300

    pagina

--- src/components/Pagina.js
+++ src/components/Pagina.js
@@ -38,19 +38,25 @@ function Pagina() {
 
             <Carousel activeIndex={index} onSelect={handleSelect}>
                 <Carousel.Item>
+
+                    <img style={{ maxHeight: "100vh" }} className="d-block w-100" src={img01} alt='First slide' />
                     <Carousel.Caption>
-                        <img style ={{ maxHeight: "100vh"}} className="d-block w-100" src={img01.jpg} alt='First slide' />
+
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdu.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
 
+                <Carousel.Item>
+                <img style={{ maxHeight: "100vh" }} className="d-block w-100" src={img02} alt='Second slide' />
                     <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
 
+                <Carousel.Item>
+                <img style={{ maxHeight: "100vh" }} className="d-block w-100" src={img03} alt='Third slide' />
                     <Carousel.Caption>
                         <h3>Third slide label</h3>
                         <p>
